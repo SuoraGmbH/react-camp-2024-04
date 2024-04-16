d35a3884ebd3297836a66c13e36f2be948dcb404
Hans-Christian Otto <c.otto@suora.com>
2024-04-16 11:04:10 +0200

    enable resetting of the form

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 
 export const TimeEntryForm: React.FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        setInputValue("");
         console.log("Hello World");
       }}
     >
       <input
         name="comment"
+        value={inputValue}
         onChange={(event) => {
           setInputValue(event.target.value);
         }}
