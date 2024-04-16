b9673b4e9d11927e470a9288502d83253cc6cddb
Hans-Christian Otto <c.otto@suora.com>
2024-04-16 16:30:55 +0200

    add time entry list state

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,41 @@ import { TimeEntryForm } from "./components/TimeEntryForm.tsx";
 import { Counter } from "./components/Counter.tsx";
 import { GithubRepoStats } from "./components/GithubRepoStats.tsx";
 import { TimeEntryList } from "./components/TimeEntryList.tsx";
+import { useState } from "react";
 
 export function App() {
+  const initialTimeEntries: TimeEntry[] = [
+    {
+      id: "46262603-e773-450e-b58c-f04853576334",
+      comment: "React lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+    {
+      id: "46262603-e773-450e-b58c-f04853576335",
+      comment: "TypeScript lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+    {
+      id: "46262603-e773-450e-b58c-f04853576338",
+      comment: "React Testing lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+  ];
+  const [timeEntries, setTimeEntries] = useState(initialTimeEntries);
+
   return (
     <>
       <TimeEntryList />
-      <TimeEntryForm onAddEntry={(timeEntry: TimeEntry) => {}} />
+      <TimeEntryForm
+        onAddTimeEntry={(timeEntry: TimeEntry) => {
+          setTimeEntries((prevTimeEntries) => {
+            return [...prevTimeEntries, timeEntry];
+          });
+        }}
+      />
       <hr />
       <GithubRepoStats />
       <Counter />
