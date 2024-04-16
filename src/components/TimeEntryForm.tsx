972b2eab98ff85465711c9c0f98f8b932d32240e
Hans-Christian Otto <c.otto@suora.com>
2024-04-16 10:54:35 +0200

    display input value next to the input field

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TimeEntryForm: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
   // Diese Typings sind eigentlich doppelt - eins von beidem würde reichen
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
     event: React.FormEvent<HTMLFormElement>,
@@ -11,7 +13,13 @@ export const TimeEntryForm: React.FunctionComponent = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="comment" />
+      <input
+        name="comment"
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      />
+      {inputValue}
       <button type="submit">Absenden</button>
     </form>
   );
