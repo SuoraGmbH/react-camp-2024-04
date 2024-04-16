f6359ee373b85a8f080c582e3824479abf6e2586
Hans-Christian Otto <c.otto@suora.com>
2024-04-16 11:31:57 +0200

    cleanup

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 
 export const Counter: React.FunctionComponent = () => {
-  // let count = 0;
   const [count, setCount] = useState(0);
-  const [incrementBy, setIncrementBy] = useState(1);
   return (
     <div>
-      <input
-        type="number"
-        value={incrementBy}
-        onChange={(event) => setIncrementBy(Number(event.target.value))}
-      />
       <button
         onClick={() => {
-          // Lange schreibweise des gleichen codes, der unten drunter steht
-          // setCount(function (prevCount) {
-          //   return prevCount + 1;
-          // });
-
-          setCount((prevCount) => prevCount + incrementBy);
+          setCount((prevCount) => prevCount + 1);
         }}
       >
         Increment
