1c12862f00775b09c6ee58c27be04f9433e01666
Hans-Christian Otto <c.otto@suora.com>
2024-04-16 10:55:28 +0200

    cleanup

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -2,23 +2,20 @@ import React, { useState } from "react";
 
 export const TimeEntryForm: React.FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
-
-  // Diese Typings sind eigentlich doppelt - eins von beidem würde reichen
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
-    event: React.FormEvent<HTMLFormElement>,
-  ) => {
-    event.preventDefault();
-    console.log("Hello World");
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log("Hello World");
+      }}
+    >
       <input
         name="comment"
         onChange={(event) => {
           setInputValue(event.target.value);
         }}
       />
+
       {inputValue}
       <button type="submit">Absenden</button>
     </form>
