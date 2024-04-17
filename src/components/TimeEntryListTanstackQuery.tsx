0e86ff7ebf7d1406b1c8ff1c5dfbb96ebe4be79a
Hans-Christian Otto <c.otto@suora.com>
2024-04-17 16:30:26 +0200

    show errors, refetch, …

--- src/components/TimeEntryListTanstackQuery.tsx
+++ src/components/TimeEntryListTanstackQuery.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+const fetchTimeEntries = async () => {
+  const response = await fetch("http://localhost:3001/timeEntries");
+
+  return response.json();
+};
 
 export const TimeEntryListTanstackQuery: React.FunctionComponent = () => {
-  const { data } = useQuery({
+  const { data, error, isError, refetch } = useQuery({
     queryKey: ["timeEntries", "all"],
-    queryFn: () => {
-      return fetch("http://localhost:3001/timeEntries").then((response) =>
-        response.json(),
-      );
-    },
+    queryFn: fetchTimeEntries,
+
+    refetchInterval: 3_000,
   });
 
-  return <pre>{JSON.stringify(data, undefined, 2)}</pre>;
+  return (
+    <pre>
+      <button onClick={() => refetch()}>🚨</button>
+      {JSON.stringify(data, undefined, 2)}
+
+      {JSON.stringify({ error, isError }, undefined, 2)}
+    </pre>
+  );
 };
