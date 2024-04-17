6b80ccf5319ab6c28a683f52c496fcff132611f9
Hans-Christian Otto <c.otto@suora.com>
2024-04-17 14:09:32 +0200

    Extract test helper

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,11 +1,19 @@
 import { beforeEach, describe, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./Counter.tsx";
-import { userEvent } from "@testing-library/user-event";
+import { userEvent, UserEvent } from "@testing-library/user-event";
 
 test("💨", () => {
   render(<Counter />);
 });
+
+async function clickIncrement(user: UserEvent) {
+  await user.click(
+    screen.getByRole("button", {
+      name: /increment/i,
+    }),
+  );
+}
 
 describe("<Counter /> functionality tests", () => {
   beforeEach(() => {
@@ -18,28 +26,15 @@ describe("<Counter /> functionality tests", () => {
 
   test("clicking the button increments the counter to 1", async () => {
     const user = userEvent.setup();
-    await user.click(
-      screen.getByRole("button", {
-        name: /increment/i,
-      }),
-    );
+    await clickIncrement(user);
 
     expect(screen.getByText(/zähler steht bei 1/i)).toBeVisible();
   });
 
   test("clicking the button twice increments the counter to 2", async () => {
     const user = userEvent.setup();
-    await user.click(
-      screen.getByRole("button", {
-        name: /increment/i,
-      }),
-    );
-
-    await user.click(
-      screen.getByRole("button", {
-        name: /increment/i,
-      }),
-    );
+    await clickIncrement(user);
+    await clickIncrement(user);
 
     expect(screen.getByText(/zähler steht bei 2/i)).toBeVisible();
   });
