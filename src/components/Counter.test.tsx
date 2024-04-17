4e079126219a1c6c7a2b130a0ac289c78aeccc6c
Hans-Christian Otto <c.otto@suora.com>
2024-04-17 14:06:34 +0200

    group tests using describe

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,42 +1,46 @@
-import { beforeEach, test } from "vitest";
+import { beforeEach, describe, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./Counter.tsx";
 import { userEvent } from "@testing-library/user-event";
 
-beforeEach(() => {
+test("💨", () => {
   render(<Counter />);
 });
 
-test("💨", () => {});
+describe("<Counter /> functionality tests", () => {
+  beforeEach(() => {
+    render(<Counter />);
+  });
 
-it("initializes the counter with zero", () => {
-  expect(screen.getByText(/zähler steht bei 0/i)).toBeVisible();
-});
+  it("initializes the counter with zero", () => {
+    expect(screen.getByText(/zähler steht bei 0/i)).toBeVisible();
+  });
 
-test("clicking the button increments the counter to 1", async () => {
-  const user = userEvent.setup();
-  await user.click(
-    screen.getByRole("button", {
-      name: /increment/i,
-    }),
-  );
+  test("clicking the button increments the counter to 1", async () => {
+    const user = userEvent.setup();
+    await user.click(
+      screen.getByRole("button", {
+        name: /increment/i,
+      }),
+    );
 
-  expect(screen.getByText(/zähler steht bei 1/i)).toBeVisible();
-});
+    expect(screen.getByText(/zähler steht bei 1/i)).toBeVisible();
+  });
 
-test("clicking the button twice increments the counter to 2", async () => {
-  const user = userEvent.setup();
-  await user.click(
-    screen.getByRole("button", {
-      name: /increment/i,
-    }),
-  );
+  test("clicking the button twice increments the counter to 2", async () => {
+    const user = userEvent.setup();
+    await user.click(
+      screen.getByRole("button", {
+        name: /increment/i,
+      }),
+    );
 
-  await user.click(
-    screen.getByRole("button", {
-      name: /increment/i,
-    }),
-  );
+    await user.click(
+      screen.getByRole("button", {
+        name: /increment/i,
+      }),
+    );
 
-  expect(screen.getByText(/zähler steht bei 2/i)).toBeVisible();
+    expect(screen.getByText(/zähler steht bei 2/i)).toBeVisible();
+  });
 });
