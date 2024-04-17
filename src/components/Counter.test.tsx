60762cb4324bad408166f2d210020cfc924f6621
Hans-Christian Otto <c.otto@suora.com>
2024-04-17 14:03:45 +0200

    add first counter tests

---
+++ src/components/Counter.test.tsx
@@ -0,0 +1,44 @@
+import { test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Counter } from "./Counter.tsx";
+import { userEvent } from "@testing-library/user-event";
+
+test("💨", () => {
+  render(<Counter />);
+});
+
+it("initializes the counter with zero", () => {
+  render(<Counter />);
+
+  expect(screen.getByText(/zähler steht bei 0/i)).toBeVisible();
+});
+
+test("clicking the button increments the counter to 1", async () => {
+  const user = userEvent.setup();
+  render(<Counter />);
+  await user.click(
+    screen.getByRole("button", {
+      name: /increment/i,
+    }),
+  );
+
+  expect(screen.getByText(/zähler steht bei 1/i)).toBeVisible();
+});
+
+test("clicking the button twice increments the counter to 2", async () => {
+  const user = userEvent.setup();
+  render(<Counter />);
+  await user.click(
+    screen.getByRole("button", {
+      name: /increment/i,
+    }),
+  );
+
+  await user.click(
+    screen.getByRole("button", {
+      name: /increment/i,
+    }),
+  );
+
+  expect(screen.getByText(/zähler steht bei 2/i)).toBeVisible();
+});
